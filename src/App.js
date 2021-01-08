1b9ccac5b08c95b4b28562ff362bfca16c8f655d
Kyle Welch <kywelch17@gmail.com>
2021-01-07 22:42:47 -0500

    firebase initialized

--- src/App.js
+++ src/App.js
@@ -1,7 +1,22 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { database } from './config/firebase';
 
 export default function App() {
-    return (
-      <h1>can_release</h1>
-    )
+  useEffect(() => {
+    database.collection("cities").doc("LA").set({
+      name: "Los Angeles",
+      state: "CA",
+      country: "USA"
+    })
+    .then(function() {
+        console.log("Document successfully written!");
+    })
+    .catch(function(error) {
+        console.error("Error writing document: ", error);
+    });
+  }, []);
+
+  return (
+    <h1>can_release</h1>
+  )
 }
