a420da619c6d879eb8b99a662071578a668fcf70
Kyle Welch <kywelch17@gmail.com>
2021-01-14 21:34:08 -0500

    styled-components setup

--- src/App.js
+++ src/App.js
@@ -1,22 +1,10 @@
-import React, { useEffect } from 'react';
-import { database } from './config/firebase';
+import React  from 'react';
+import { HeaderContainer } from './container/header';
+//import { database } from './config/firebase';
+// 
 
 export default function App() {
-  useEffect(() => {
-    database.collection("cities").doc("LA").set({
-      name: "Los Angeles",
-      state: "CA",
-      country: "USA"
-    })
-    .then(function() {
-        console.log("Document successfully written!");
-    })
-    .catch(function(error) {
-        console.error("Error writing document: ", error);
-    });
-  }, []);
-
-  return (
-    <h1>can_release</h1>
-  )
+    return (
+        <HeaderContainer />
+    )
 }
