3d6f2cf97a98c799ab02a875d8b1fb9a8ec567fd
Kyle Welch <kywelch17@gmail.com>
2021-01-19 13:04:06 -0500

    forgot this

---
+++ src/pages/landing.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { HeaderContainer } from '../container/header';
+
+const Landing = () => {
+    return (
+        <HeaderContainer />
+    );
+}
+
+export default Landing;
