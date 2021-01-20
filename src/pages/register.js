44a9996378cebd8ba9f519c6b45f2e11a4c95ba7
Kyle Welch <kywelch17@gmail.com>
2021-01-19 22:04:16 -0500

    register and process complete

--- src/pages/register.js
+++ src/pages/register.js
@@ -1,94 +1,118 @@
 import React, { useState } from 'react';
 import firebase from 'firebase';
 import { useHistory, Link } from 'react-router-dom';
+import { Cookies} from 'react-cookie';
 
-import { database } from '../lib/firebase';
+import { database } from '../config/firebase';
+import { registerErrors } from '../helper/error';
 import * as ROUTES from '../constants/routes';
 import Form from '../components/form/index';
 
 const Register = () => {
+    const [type, setType] = useState('');
     const [name, setName] = useState('');
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
+    const [confirmPassword, setConfirmPassword] = useState('');
     const [error, setError] = useState([]);
 
     const history = useHistory();
 
-    const handleRegister = async (e) => {
+    const handleRegister = (e) => {
         e.preventDefault();
 
-        try {
-            let user = {
-                name: name,
-                email: email,
-                password: password
-            };
-            let ref = database.collection('users');
-            let userRef = ref.doc(name.replace(/\s/g, ''));
-            await userRef.set({
-                user,
-                createdAt: firebase.firestore.FieldValue.serverTimestamp(),
-                updatedAt: firebase.firestore.FieldValue.serverTimestamp()
+        let user = {
+            type: type,
+            name: name,
+            email: email,
+            password: password,
+            confirmPassword: confirmPassword
+        };
+
+        setError(registerErrors(user));
+        if (error.length > 0) {
+            console.log(error);
+            return;
+        }
+
+        database.collection('users').where('user.email', '==', email).get()
+            .then(snapshot => {
+                if (snapshot.size > 0) {
+                    setError([...error, 'Already exists']);
+                    console.log(error);
+                    return;
+                }
+                else {
+                    database.collection('users').doc(Math.random().toString(16).substr(2, 8)).set({
+                        user,
+                        createdAt: firebase.firestore.FieldValue.serverTimestamp(),
+                        updatedAt: firebase.firestore.FieldValue.serverTimestamp()
+                    })
+                        .then(res => {
+                            console.log('set: ' + res);
+                        })
+                        .catch(err => {
+                            console.error(err);
+                        })
+                }
+            })
+            .catch(err => {
+                console.error(err);
             });
-            history.push(ROUTES.LANDING);
-        }
-        catch(error) {
-            console.error(error);
-        }
     }
 
     return (
         <>
             <Form>
-                <Form.Base method='POST'>
+                <Form.Base>
                     <Form.Title>Register</Form.Title>
-                    { error && <Form.Error></Form.Error>}
-                    <Form.User
-                        name='user'
-                        id='user'
-                    >
-                        <option value='brewery'>Brewery</option>
-                        <option value='brewery'>Personal</option>
-                        <option value='brewery'>Resale</option>
-                    </Form.User>
-                    <Form.Label>Name</Form.Label>
-                    <Form.Input
-                        type='text'
-                        value={name}
-                        onChange={({ target }) => setName(target.value)}
-                        id='name'
-                        placeholder="Bruce Wayne"
-                    />
-                    <Form.Label>Email Address</Form.Label>
-                    <Form.Input
-                        type='email'
-                        value={email}
-                        onChange={({ target }) => setEmail(target.value)}
-                        id='email'
-                        placeholder="bwayne@gotham.com"
-                    />
-                    <Form.Label>Password</Form.Label>
-                    <Form.Input
-                        type='password'
-                        value={password}
-                        onChange={({ target }) => setPassword(target.value)}
-                        id='password'
-                        placeholder="Batcave1232$"
-                    />
-                    <Form.Switch id='register'>Already have an account?</Form.Switch>
-                    <Form.Submit
-                        type='submit'
-                        onClick={handleRegister}
-                    >Submit</Form.Submit>
-                    <Form.AltText>Register with:</Form.AltText>
-                    <Form.Alt>
-                        <a href='https://facebook.com'>
+                    <Form.Sheet>
+                        <Form.Select
+                            id='type'
+                            value={type}
+                            onChange={({ target }) => setType(target.value)}
+                        >
+                            <option value='brewery'>Brewery</option>
+                            <option value='resale'>Resale</option>
+                            <option value='personal'>Personal</option>
+                        </Form.Select>
+                        <Form.Input 
+                            id='name'
+                            type='text'
+                            value={name}
+                            onChange={({ target }) => setName(target.value)}
+                            placeholder='Name'
+                        />
+                        <Form.Input 
+                            id='email'
+                            type='email'
+                            value={email}
+                            onChange={({ target }) => setEmail(target.value)}
+                            placeholder='Email Address'
+                        />
+                        <Form.Input 
+                            id='password'
+                            type='password'
+                            value={password}
+                            onChange={({ target }) => setPassword(target.value)}
+                            placeholder='Password'
+                        />
+                        <Form.Input 
+                            id='confirmPassword'
+                            type='password'
+                            value={confirmPassword}
+                            onChange={({ target }) => setConfirmPassword(target.value)}
+                            placeholder='Password Confirm'
+                        />
+                    </Form.Sheet>
+                    <Form.Submit onClick={handleRegister}>Submit</Form.Submit>
+                    <Form.Alternative>
+                        <Form.AltTitle>Register with:</Form.AltTitle>
+                        <div>
                             <Form.AltIcon id='facebook' />
-                        </a>
-                        <a href='https://google.com'>
                             <Form.AltIcon id='google' />
-                        </a>
-                    </Form.Alt>
+                        </div>
+                    </Form.Alternative>
                 </Form.Base>
             </Form>
         </>
